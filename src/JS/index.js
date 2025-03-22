6bd9d1b398ba35b0229ce93e76d078ce5d059710
mazenDev <mazenfathy841@gmail.com>
2025-03-22 21:58:32 +0200

    repair final proplem now

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,13 +4,8 @@
 ########################
 */
 /*First: Importing Modules And  libraries*/
-
 import { changeVisabilty } from "./utils/password-visabilty.js";
 import { changeMode } from "./utils/change-mode.js";
-
-import { changeVisabilty } from "./utilites/password-visabilty.js";
-import { changeMode } from "./utilites/change-mode.js";
-
 /*Second : The Main code*/
 changeVisabilty();
 changeMode();
