6f3b8672df5220a701d0780310dbe4a15153673a
mazenDev <mazenfathy841@gmail.com>
2025-03-23 00:18:24 +0200

    check from the lenght of the input value

---
+++ src/JS/auth/length-auth.js
@@ -0,0 +1,27 @@
+export const checkLength = () => {
+  try {
+    const inputFieldsContainer = document.querySelectorAll(".js-input-field");
+    inputFieldsContainer.forEach((container) => {
+      const inputField = container.querySelector("input");
+      const errorMsgSpan = container.querySelector(".js-error-msg");
+      if (
+        inputField.type === "tel" &&
+        inputField.value.length < 11 &&
+        inputField.value.length !== 0
+      ) {
+        errorMsgSpan.innerText = `You Entered ${inputField.value.length} You must Enter 11 Number`;
+        errorMsgSpan.classList.add("visable");
+      } else if (
+        inputField.type === "password" ||
+        inputField.type === "text"&&
+        inputField.value.length < 8 &&
+        inputField.value.length !== 0
+      ) {
+        errorMsgSpan.innerText = `You Entered ${inputField.value.length} You must Enter at least 8 Numbers`;
+        errorMsgSpan.classList.add("visable");
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/JS/index.js
+++ src/JS/index.js
@@ -8,10 +8,12 @@ import { changeVisabilty } from "./utils/password-visabilty.js";
 import { changeMode } from "./utils/change-mode.js";
 import { checkFormFields } from "./auth/empty-auth.js";
 import { filterInput } from "./utils/filter-input.js";
+import { checkLength } from "./auth/length-auth.js";
 /*Second : The Main code*/
 changeVisabilty();
 changeMode();
 filterInput();
 document.querySelector("button").addEventListener("click", () => {
   checkFormFields();
+  checkLength();
 });
