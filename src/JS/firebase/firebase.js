a737b02e5a818c5668096570503187d6222b132d
mazenDev <mazenfathy841@gmail.com>
2025-03-22 21:46:10 +0200

    update folder  tree in src

--- src/JS/firebase/firebase.js
+++ src/JS/firebase/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebase-config.js";
+import { firebaseConfig } from "../firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 console.log(app)
--- src/JS/index.js
+++ src/JS/index.js
@@ -4,8 +4,8 @@
 ########################
 */
 /*First: Importing Modules And  libraries*/
-import { changeVisabilty } from "./password-visabilty.js";
-import { changeMode } from "./change-mode.js";
+import { changeVisabilty } from "./utilites/password-visabilty.js";
+import { changeMode } from "./utilites/change-mode.js";
 /*Second : The Main code*/
 changeVisabilty();
 changeMode();
